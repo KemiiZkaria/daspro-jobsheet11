73b15d8ddbfb3c70e55ff6c4334d383abc5f799f
KemiiZkaria <ahmadkevinmalik01@gmail.com>
2024-11-21 10:04:49 +0700

    Melakukan push pertanyaan percobaan 2

--- Kafe04.java
+++ Kafe04.java
@@ -1,9 +1,16 @@
 public class Kafe04 {
-    public static void Menu(String namaPelanggan, boolean isMember) {
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
 
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Anda mendapatkan diskon sebesar 50%");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Anda mendapatkan diskon sebesar 30%");
+        } else {
+            System.out.println("Invalid");
         }
         System.out.println("==== MENU RESTO KAFE ====");
         System.out.println("1. Kopi Hitam - RP 15,000");
@@ -16,6 +23,6 @@ public class Kafe04 {
         System.out.println("Silahkan pilih menu yang anda inginkan.");
     }
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Andi", true, "DISKON50");
     }
 }
