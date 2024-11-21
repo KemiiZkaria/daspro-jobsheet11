b51a1301903c02180e035e7b7a74afeddaa09c51
KemiiZkaria <ahmadkevinmalik01@gmail.com>
2024-11-21 16:26:39 +0700

    Melakukan push percobaan 5

--- PengunjungCafe04.java
+++ PengunjungCafe04.java
@@ -1,11 +1,11 @@
 public class PengunjungCafe04 {
-    public static void daftarPengunjung(String... namaPengunjung){
+    public static void daftarPengunjung(int jumlahPengunjung, String... namaPengunjung){
         System.out.println("Daftar Nama Pengunjung:");
         for (String nama : namaPengunjung) {
             System.out.println("- " + nama);
         }
     }
     public static void main(String[] args) {
-        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung(3, "Ali", "Budi", "Citra");
     }
 }
