5eb3cafff92cd93665561504410a2bdac1a5591e
KemiiZkaria <ahmadkevinmalik01@gmail.com>
2024-11-21 09:00:49 +0700

    Melakukan push pertanyaan 2

--- Kafe04.java
+++ Kafe04.java
@@ -1,6 +1,5 @@
 public class Kafe04 {
-
-    public static void menu() {
+    public static void main(String[] args) {
         System.out.println("==== MENU RESTO KAFE ====");
         System.out.println("1. Kopi Hitam - RP 15,000");
         System.out.println("2. Cappuccino - RP 20,000");
@@ -9,9 +8,6 @@ public class Kafe04 {
         System.out.println("5. Roti Bakar - RP 10,000");
         System.out.println("6. Mie Goreng - RP 18,000");
         System.out.println("=========================");
-        System.out.println("Silahkan pilih menu yang anda inginkan.");
-    }
-    public static void main(String[] args) {
-        menu();
+        System.out.println("Silahkan pilih menu yang anda inginkan.");        
     }
 }
