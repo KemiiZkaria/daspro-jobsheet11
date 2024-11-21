270de86016830cd415e78900a0915fb49ee74982
KemiiZkaria <ahmadkevinmalik01@gmail.com>
2024-11-21 10:17:44 +0700

    Melakukan push percobaan 3

--- Kafe04.java
+++ Kafe04.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 public class Kafe04 {
+    
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
+    }
+
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
-
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
         }
@@ -23,6 +30,16 @@ public class Kafe04 {
         System.out.println("Silahkan pilih menu yang anda inginkan.");
     }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         Menu("Andi", true, "DISKON50");
+        System.out.print("\nMasukkan nomor menu yang ingin anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin anda pesan: ");
+        int banyakItem = sc.nextInt();
+        
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan anda: Rp." + totalHarga);
     }
 }
