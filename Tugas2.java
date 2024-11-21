3ef1c170ce6afb9640ce579e688458549f1a40c6
KemiiZkaria <ahmadkevinmalik01@gmail.com>
2024-11-21 18:33:56 +0700

    Melakukan push tugas 2

--- Tugas2.java
+++ Tugas2.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
 public class Tugas2 {
-    static int[][] penjualan = new int[5][7]; // Array 5 menu x 7 hari
+    static int[][] penjualan = new int[5][7];
     static String[] menu = {"Kopi", "Teh", "Es Degan", "Roti Bakar", "Gorengan"};
 
-    // Fungsi untuk menginputkan data penjualan
     public static void inputData() {
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < menu.length; i++) {
@@ -17,18 +16,16 @@ public class Tugas2 {
         }
     }
 
-    // Fungsi untuk menampilkan seluruh data penjualan
     public static void tampilkanData() {
-        for (int j = 0; j < 7; j++) { // Iterasi per hari
+        for (int j = 0; j < 7; j++) {
             System.out.println("Pada Hari ke-" + (j + 1) + ":");
-            for (int i = 0; i < menu.length; i++) { // Iterasi untuk setiap menu
-                System.out.println(menu[i] + " terjual sebanyak " + penjualan[i][j] + " unit.");
+            for (int i = 0; i < menu.length; i++) {
+                System.out.println(menu[i] + " terjual sebanyak " + penjualan[i][j]);
             }
-            System.out.println(); // Baris kosong untuk memisahkan setiap hari
+            System.out.println();
         }
     }
 
-    // Fungsi untuk menampilkan menu dengan penjualan tertinggi
     public static void menuTertinggi() {
         int maxPenjualan = 0;
         String menuTerlaris = "";
@@ -42,10 +39,9 @@ public class Tugas2 {
                 menuTerlaris = menu[i];
             }
         }
-        System.out.println("Menu dengan penjualan tertinggi adalah: " + menuTerlaris + " dengan total penjualan: " + maxPenjualan);
+        System.out.println("Menu dengan penjualan tertinggi adalah " + menuTerlaris + " dengan total penjualan " + maxPenjualan);
     }
 
-    // Fungsi untuk menampilkan rata-rata penjualan untuk setiap menu
     public static void rataRataPenjualan() {
         System.out.println("Rata-rata penjualan untuk setiap menu:");
         for (int i = 0; i < menu.length; i++) {
@@ -61,16 +57,12 @@ public class Tugas2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input data penjualan
         inputData();
 
-        // Menampilkan data
         tampilkanData();
 
-        // Menampilkan menu dengan penjualan tertinggi
         menuTertinggi();
 
-        // Menampilkan rata-rata penjualan
         rataRataPenjualan();
     }
 }
